e7e937b251d827fde91625880e93e465ffcfd8c6
Hitesh Kumawat <kumawat.hitesh743@gmail.com>
2021-02-07 11:32:57 +0530

    react frontend initial commit

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React, {Fragment, useEffect} from 'react';
+import Navbar from './component/layout/Navbar';
+import Landing from './component/layout/Landing';
+import Login from './component/auth/Login';
+import Alert from './component/layout/Alert';
+import Register from './component/auth/Register';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './component/dashboard/Dashboard';
+import CreateProfile from './component/profile-forms/CreateProfile';
+import EditProfile from './component/profile-forms/EditProfile';
+import AddExperience from './component/profile-forms/AddExperience';
+import AddEducation from './component/profile-forms/AddEducation';
+import PrivateRoute from './component/routing/PrivateRoute';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import {loadUser} from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+import './App.css';
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
+const App = () => { 
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return(
+  <Provider store = {store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+            <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+            <PrivateRoute exact path='/add-experience' component={AddExperience} />
+            <PrivateRoute exact path='/add-education' component={AddEducation} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+)};
+
+export default App;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 //@route   Get api/profile/me
 //@desc    Test route
@@ -148,7 +149,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access   Private
 router.delete('/', auth, async (req, res) => {
     try {
-      // @todo Remove user posts
+      // Remove user posts
+      await Post.deleteMany({ user: req.user.id });
       // Remove profile
       await Profile.findOneAndRemove({ user: req.user.id });
       // Remove user
